1c2e500f71868b4c605dd2013e1cfa24f8cac8ce
Abdurrahman <abdoarafh54@gmail.com>
2022-10-14 14:15:23 +0200

    added: vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "/dice-shuffle/",
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
